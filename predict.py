3686a95a86ffbcfcb65b965c80eb9810bfc14e60
SA3i2022 <107735382+SA3i2022@users.noreply.github.com>
2023-12-26 16:36:42 -0800

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,13 @@
+from prediction_cubes import uncubify
+def predict_w_model(cubed_array, model, pad_size, original_shape):
+  a,c,d,e=np.shape(cubed_array)
+  sav=[]
+  b=range(0,a,1)
+  for i in b:
+    test_image=padded_array2[i:i+1]
+    out=model.predict(test_image)
+    sav.append(out)
+  sav=np.squeeze(sav)
+  seeit=sav[:,0:c-pad_size,0:d-pad_size,0:e-pad_size]
+  see=uncubify(seeit, original_shape)
+  return see
